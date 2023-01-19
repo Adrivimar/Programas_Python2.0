ad6aaccf1eafc35ed6d21f52e3df758f30407ac6
Adrivimar <adrivillalon10@gmial.com>
2023-01-19 21:12:50 +0100

    Primer comiteo nuevo repositorio

---
+++ Python/Pruebas.py
@@ -0,0 +1,9 @@
+#DIVISIONES
+cifra1 = int(input("Primera cifra"))
+cifra2 = int(input("Segunda cifra"))
+resto = cifra1 % cifra2
+resultado = cifra1 / cifra2
+print ("El resultado de" + str(cifra1) + str(cifra2) + " es " + str(resultado) + " y el resto " + str(resto))
+
+
+
